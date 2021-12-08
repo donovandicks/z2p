aead279977f33f4df6425b756387aaa1eed1b886
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-07 23:34:40 -0500

    Updated to parameterize the address for the app

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 /// Check the status of the server to see if it is running
 ///
@@ -10,14 +11,19 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-/// Builds and returns a server with all routes and middleware
+/// Builds and returns a server with all routes and middleware, listening on the
+/// given listener
+///
+/// # Args
+///
+/// * `listener` - A `TcpListener` that is already bound to an address
 ///
 /// # Returns
 ///
 /// * `Ok(Server)` if the build process is successful
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,33 @@
-fn spawn_app() {
-    let server = z2p::run().expect("Failed to bind to address");
+use std::net::TcpListener;
+
+/// Spawns the app as a background process on a random port so it can be used
+/// for testing
+///
+/// # Returns
+///
+/// * The address on which the app is listening
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = z2p::run(listener).expect("Failed to bind to address");
 
     // Spawn the server as a background task, allowing it to run concurrently
     // with downstream futures and tasks, including tests
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[actix_rt::test]
 async fn health_check_works() {
     // Setup
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Test
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to send request");
