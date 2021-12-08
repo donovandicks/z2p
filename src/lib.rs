862a99be07f2cdb60a45a5966f73027c34bc7e60
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-08 00:45:30 -0500

    Started subscriptions handler

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,16 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 /// Builds and returns a server with all routes and middleware, listening on the
 /// given listener
 ///
@@ -22,9 +32,13 @@ async fn health_check() -> HttpResponse {
 ///
 /// * `Ok(Server)` if the build process is successful
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
