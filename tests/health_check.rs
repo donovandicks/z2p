017ac84a88c4227ea894ccd7ebf56113f79dee78
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-07 23:53:38 -0500

    Add tests for subscriptions endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -35,3 +35,51 @@ async fn health_check_works() {
     assert!(response.status().is_success()); // 200 status
     assert_eq!(Some(0), response.content_length()); // No content
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Setup
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=skinny%20pete&email=skinny_pete%40gmail.com";
+
+    // Test
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=skinny%20pete", "missing the email"),
+        ("email=skinny_pete%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Custom message triggered if the test fails
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
+}
