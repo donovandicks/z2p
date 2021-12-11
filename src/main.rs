0d8fe2cf6051bec6fa6bc1a1f5723242eaecc01d
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-11 15:08:56 -0500

    Added envlogger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use z2p::{configuration::get_configuration, startup::run};
@@ -5,6 +6,9 @@ use z2p::{configuration::get_configuration, startup::run};
 /// Runs the web server
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Fallback to >= info level if the RUST_LOG variable has not been set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("Failed to read configuration.");
     let conn_pool = PgPool::connect(&config.database.connection_string())
         .await
